09de421ab2268be2fd360a99e252164fd262f0f5
Abdur Rahman Fahim <127137394+abdurrahmanfahim@users.noreply.github.com>
2025-04-08 03:10:26 +0600

    favicon changed

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,16 +1,11 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
 import React from "react";
 
 import Card from "./Card";
 import cardData from "../assets/cardData";
 
 const Blog = () => {
+  const { blog_articles } = cardData;
 
-  const {blog_articles} = cardData
-  console.log(blog_articles);
-  
- 
   return (
     <>
       <section className="w-full bg-white relative z-40 pb-24">
@@ -24,13 +19,20 @@ const Blog = () => {
             </p>
           </div>
           <div className="cards flex mt-6">
-
-          {
-            blog_articles.map( (article, index) => (
-              <Card key={index} style={'blogCard'} img={article.img} heading={article.title} paragraph={article.description} button={article.button} time={article.date} commentCount={article.comments} name={article.author} />
-            ))
-          }
-        </div>
+            {blog_articles.map((article, index) => (
+              <Card
+                key={index}
+                style={"blogCard"}
+                img={article.img}
+                heading={article.title}
+                paragraph={article.description}
+                button={article.button}
+                time={article.date}
+                commentCount={article.comments}
+                name={article.author}
+              />
+            ))}
+          </div>
         </div>
       </section>
     </>
